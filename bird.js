5834fae5dc44070054ae528b2053550ea86a782a
Jake Kugel <jake_kugel@yahoo.com>
2022-04-03 15:33:44 -0500

    Adding gravity

--- bird.js
+++ bird.js
@@ -8,9 +8,22 @@ class Bird {
         this.canvas = canvas;
 
         this.flapsPerSecond = 3;
+        this.gravityStrength = 2;
+
+        this.bounceY = x;
+        this.bounceTime = -1;
+    }
+
+    flap(time) {
+        this.bounceY = this.calculateY(time);
+        this.bounceTime = time;
     }
 
     draw(time) {
+        if (this.bounceTime === -1) {
+            this.bounceTime = time;
+        }
+
         // Determine which animation frame
         const frame = Math.floor(((time * this.flapsPerSecond) % 1000) / 167);
 
@@ -18,7 +31,14 @@ class Bird {
         let clipx = (frame % 3) * 1024;
         let clipy = (frame > 3) ? 926 : 0;
 
+        this.y = this.calculateY(time);
+
         let ctx = this.canvas.getContext("2d");
         ctx.drawImage(this.bitmap,clipx, clipy, 1024, 926, this.x, this.y, 128, 112);
+    }
+
+    calculateY(time) {
+        const elapsed = time - this.bounceTime;
+        return this.bounceY - (elapsed / 1000) * 300 * this.gravityStrength + Math.pow((elapsed / 1000), 2) * 300 * this.gravityStrength;
     }
 }
